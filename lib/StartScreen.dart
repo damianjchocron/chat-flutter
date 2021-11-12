25bc48ca5d97b4fa3bd0ebd070a609a28ce52655
Damian J. Chocron Martinez <z3n4.cs@gmail.com>
2021-11-12 23:20:11 +0100

    feat(lib StartScreen): add Scaffold class with backgroudColor

--- lib/StartScreen.dart
+++ lib/StartScreen.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFF5F72E6),
+    );
   }
 }
