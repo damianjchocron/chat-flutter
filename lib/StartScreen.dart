89069264ec6b78db2bd93dc85751ca7adbafe5e4
Damian J. Chocron Martinez <z3n4.cs@gmail.com>
2021-11-14 19:38:40 +0100

    mend

--- lib/StartScreen.dart
+++ lib/StartScreen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+/* Logo declaration */
+final String assetName = 'assets/images/send.png';
+final Widget logo = Image(image: AssetImage(assetName));
+
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0XFF5F72E6),
-    );
+        backgroundColor: Color(0XFF5F72E6), body: Center(child: logo));
   }
 }
